ffe4492f2032465087afb2e382d6b39e936a25ec
Danilo Gasques <danilod100@gmail.com>
2013-11-24 19:39:29 -0200

    Trying 12550 again

--- uva/12550.cpp
+++ uva/12550.cpp
@@ -1,9 +1,9 @@
 //http://uva.onlinejudge.org/external/125/p12550.pdf
-//#NOTSOLVED# #RUNTIMEERROR#
+//Thanks to a friend that told me that RuntimeError exceptions include DivisionByZero! I got stuck in this problem because of it
+//#NOTSOLVED #WRONGANSWER
 #include <iostream>
 #include <string>
 #include <sstream>
-#include <climits>
 
 using namespace std;
 
@@ -15,68 +15,87 @@ int main(int argc, char *argv[])
     //reads each line at once
     while(getline(cin,line))
     {
-      stringstream ss(line);
+        stringstream ss(line);
 
-      int P,D;
-      ss >> D;
-      ss >> P;
+        int P,D;
+        ss >> D;
+        ss >> P;
 
-      //read data
-      const int d1 = D+1;
-      unsigned int *total= new unsigned int[d1];
-      int count=0;
-      unsigned int tmp;
-      while( ss >> tmp)
-          total[count++] =tmp;
+        //read data
+        const int d1 = D+1;
+        unsigned int total[d1];
+        int count=0;
+        unsigned int tmp;
+        while( ss >> tmp)
+            total[count++] =tmp;
 
-      if(count < d1)
-      {
-
-        int a=1,b=1;
-
-        //try to understand the pattern
-        if(count >= 4)
+        if(count < d1)
         {
-          if(total[0] == 0)   //simple case (a*v1 + b*v2, where v2 = 0)
-          {
-            a = total[2] / total[1]; //total[2]  = a * total[1] + b * total[0]
-            // total[n] = a*total[n-1] +b*total[n-2]
-            b = (total[3] - a*total[2])/total[1];
-          } else { // 'complex case'
-          
-          }
+
+            int a=1,b=1;
+
+            // -3 -2 -1 0
+            // v1 v2 v3 v4
+            // 0   1  2  3
+            //
+            // v4 = a*v3 + b*v2
+            // v3 = a*v2 + b*v1
+            // a*v3 = v4 - b*v2 --> a*v3 = v4 - (v3*v2 - a*v2*v2)/v1 --> (a*v3 +a*v2*v2 )/v1 = v4 - v3*v2 --> a(v3 - v2^2)/v1 = v4 - v3*v2 --> a = (v1*v4  - v2*v3)/(v3*v1 - v2*v2)
+            // b*v1 = v3 - a*v2 --> b = (v3 - a*v2)/v1
+
+            --count;
+
+            //binary search for zeros
+
+            //last one is zero? (first and second are zeros)
+            if(total[count] == 0)
+            {
+                a = b =0;
+            }
+            else
+                //'first' one is zero
+                if(total[0] == 0)
+                {
+                    //v3 = a*v2 + b*v1 = a*v2 --> a = v3/v2
+                    a =  total[2]/total[1];
+                    //v4 = a*v3 + b*v2 --> (v4 - b*v2)/v3
+                    b = (total[3] - a*total[1])/total[2];
+                }
+                else //no zeros :(
+                {
+                    a = (total[0]*total[3] - total[1]*total[2])/(total[2]*total[0] - total[1]*total[1]);
+                    b = (total[2] - a*total[1])/total[0];
+                }
+
+
+            //generate remaining data
+            for(int i=count; i < d1; ++i)
+            {
+                total[i] = (a*total[i-1] + b*total[i-2]);
+                if(total[i] < total[i-1]) //shitty way of dealing with overflows
+                    total[i] = P+1;
+            }
+
         }
 
-        //generate remaining data
-        for(int i=count; i < d1; ++i)
+        // result
+        if(P >= total[D])
         {
-          total[i] = (a*total[i-1] + b*total[i-2]);
-          if(total[i] < total[i-1]) //shitty way of dealing with overflows
-              total[i] = P+1;
-        }
-       }
+            cout << "The spider may fall!" << endl;
+        } else {
 
-      // result
-
-      if(P >= total[D])
-      {
-          cout << "The spider may fall!" << endl;
-      } else {
-
-          if(P <= total[0])
-              cout << "The spider is going to fall!" << endl;
-          else
-              {
+            if(P <= total[0])
+                cout << "The spider is going to fall!" << endl;
+            else
+            {
                 int distance = 1;
-                while(distance <D-1 && total[D - distance++] > P);
+                while(distance <= D-1 && total[D - distance++] > P);
                 --distance;
 
                 cout << distance << endl; 
-              }
-      }
-
-      delete[] total;
-   }
+            }
+        }
+    }
 
     return 0;
 }
